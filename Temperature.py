112ae9c320d830e26d87f5da10816542921c243b
Sam Escott <6706111+sme272@users.noreply.github.com>
2020-04-24 21:27:58 +0200

    Temperature reading

---
+++ Temperature.py
@@ -0,0 +1,72 @@
+#! /usr/bin/python3.8
+import tkinter as tk
+from random import choice
+from math import pi
+
+
+def kelvin(temp):
+    return temp + 273.15
+
+
+def rankine(temp):
+    temp += 273.15
+    return temp * 9 / 5
+
+
+def delisle(temp):
+    temp = 100 - temp
+    return temp * 3 / 2
+
+
+def newton(temp):
+    return temp * 33 / 100
+
+
+def reaumur(temp):
+    return temp * 4 / 5
+
+
+def romer(temp):
+    temp = temp * 21 / 40
+    return temp + 7.5
+
+
+def radians(temp):
+    return temp * pi / 180
+
+
+def random_unit(temp):
+    units = choice(list(scales.keys()))
+    new_temp = scales[units](temp)
+    if units == "Radians":
+        lbl_text = f"{new_temp:.2f} radians"
+    else:
+        lbl_text = f"{new_temp:.2f} degrees {units}"
+    lbl.configure(text=lbl_text)
+
+
+scales = {
+    "Kelvin": kelvin,
+    "Rankine": rankine,
+    "Delisle": delisle,
+    "Newton": newton,
+    "Reaumur": reaumur,
+    "Romer": romer,
+    "Radians": radians,
+}
+
+window = tk.Tk()
+
+window.title("Temperature")
+window.geometry("250x100")
+
+temp = 30
+
+lbl = tk.Label(window)
+lbl.pack(ipady=15)
+random_unit(temp)
+
+btn_units = tk.Button(window, text="Change units", command=lambda: random_unit(temp))
+btn_units.pack()
+
+window.mainloop()
